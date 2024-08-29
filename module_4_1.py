74b00367fcd2809e6c37eae1397a9bc67fbef25b
3vk1r <135658009+3vk1r@users.noreply.github.com>
2024-08-29 14:27:59 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import devide as fake_d
+from true_math import devide as true_d
+
+result1 = fake_d(69, 3)
+result2 = fake_d(3, 0)
+result3 = true_d(49, 7)
+result4 = true_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
